bf6edeae958d6de2673d89ad7e04d4b0a36b9902
jason315 <lyna318@163.com>
2025-04-24 09:32:20 +0800

    support login user in server module

--- server/src/router/user.js
+++ server/src/router/user.js
@@ -3,6 +3,7 @@ const md5 = require('blueimp-md5')
 const UserModel = require('../db/models')
 
 const router = express.Router()
+const withoutPasswordFilter = {password: 0}
 
 router.post('/register', (req, res) => {
   const {username, password, type} = req.body
@@ -34,6 +35,30 @@ router.post('/register', (req, res) => {
           'data': null,
           'msg': err.message
         })
+      })
+    }
+  })
+})
+
+router.post('/login', (req, res) => {
+  const {username, password} = req.body
+  UserModel.findOne({ username, password: md5(password) }, withoutPasswordFilter)
+  .then(user => {
+    if (!user) {
+      res.json({
+        'code': 1,
+        'data': null,
+        'msg': 'Invalid username or password.'
+      })
+    } else {
+      res.cookie('userid', user._id, { maxAge: 60 * 60 * 1000 * 24 * 7 })
+      res.json({
+        'code': 0,
+        'data': {
+          _id: user._id,
+          username
+        },
+        'msg': 'sucess'
       })
     }
   })
